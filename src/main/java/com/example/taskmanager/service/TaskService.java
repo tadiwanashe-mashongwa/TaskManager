be9d8988fd0bc044a5f87bd802c9125834bfaef2
Tadiwanashe Mashongwa <mashongwatadiwanashe@gmail.com>
2026-06-11 15:02:41 +0200

    test(service): implement and verify getAllTasks via explicit ArrayList mapping

--- src/main/java/com/example/taskmanager/service/TaskService.java
+++ src/main/java/com/example/taskmanager/service/TaskService.java
@@ -5,8 +5,11 @@ import com.example.taskmanager.dto.TaskResponseDTO;
 import com.example.taskmanager.entity.Task;
 import com.example.taskmanager.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -31,5 +34,33 @@ public class TaskService {
                         savedTask.getUpdatedAt()
                 );
 
+    }
+    public TaskResponseDTO findTaskById(Long id){
+       Task savedTask =taskRepository.findById(id).orElseThrow(()->new RuntimeException("not found"));
+        return new TaskResponseDTO(
+                savedTask.getId(),
+                savedTask.getTitle(),
+                savedTask.getDescription(),
+                savedTask.getStatus(),
+                savedTask.getDueDate(),
+                savedTask.getCreatedAt(),
+                savedTask.getUpdatedAt()
+        );
+    }
+    public List<TaskResponseDTO> findAllTasks(){
+        List<Task> tasks=taskRepository.findAll();
+        List<TaskResponseDTO> taskResponseDTOS=new ArrayList<>();
+        tasks.forEach(task -> {
+           taskResponseDTOS.add(new TaskResponseDTO(
+                    task.getId(),
+                    task.getTitle(),
+                    task.getDescription(),
+                    task.getStatus(),
+                    task.getDueDate(),
+                    task.getCreatedAt(),
+                    task.getUpdatedAt()
+            ));
+        });
+        return taskResponseDTOS;
     }
 }
