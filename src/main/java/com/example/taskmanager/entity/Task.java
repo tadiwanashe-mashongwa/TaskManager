b8903a4250650e1d4e26e97600419e304043bd14
Tadiwanashe Mashongwa <mashongwatadiwanashe@gmail.com>
2026-06-11 10:46:36 +0200

    feat(task):create core database peristence entity

---
+++ src/main/java/com/example/taskmanager/entity/Task.java
@@ -0,0 +1,32 @@
+package com.example.taskmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private Instant dueDate;
+
+
+}
